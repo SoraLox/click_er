2bf3d7a2c57721ebd70f57948c154df0f5c59fa4
SoraLox <onegreg0ry4k@gmail.com>
2021-10-26 11:44:00 +0300

    lol

--- main.py
+++ main.py
@@ -2,27 +2,32 @@ from ctypes import pythonapi
 import pyautogui
 import time
 
+
 # 1
 def cursor_position():
     print(pyautogui.position())
 
+
 # 2
 def cursor_position_time(time1, inter):
-    i=0
+    i = 0
     while time1 >= i:
         print(pyautogui.position())
         i += 1
         time.sleep(inter)
 
+
 # 3
 def screen_size():
     print(pyautogui.size())
 
+
 def move_to(x, y, duration):
     pyautogui.moveTo(x, y, duration)
 
+
 def click_er(x, y, clicks, interval):
-    pyautogui.click(x=x, y=y, clicks=clicks, interval=interval, button='left' )
+    pyautogui.click(x=x, y=y, clicks=clicks, interval=interval, button='left')
     #  >>> pyautogui.click(x=moveToX, y=moveToY, clicks=num_of_clicks, interval=secs_between_clicks, button='left')
 
 
@@ -45,12 +50,10 @@ while True:
         cursor_position()
         input("Press Enter to continue...")
 
-    
     if option_1 == 2:
-        
         print('how many times will check')
         option_2_1 = int(input())
-        
+
         print('interval behind checks')
         option_2_2 = int(input())
 
@@ -58,15 +61,12 @@ while True:
 
         input("Press Enter to continue...")
 
-
     if option_1 == 3:
         screen_size()
         input("Press Enter to continue...")
 
-
     if option_1 == 4:
-
-        print('x position')   
+        print('x position')
         option_4_1 = int(input())
 
         print('y position')
@@ -74,15 +74,13 @@ while True:
 
         print('duration in sec how long will go to the point')
         option_4_3 = int(input())
-        
+
         move_to(option_4_1, option_4_2, option_4_3)
 
         input("Press Enter to continue...")
 
-    
     if option_1 == 5:
-        
-        print('x position')   
+        print('x position')
         option_5_1 = int(input())
 
         print('y position')
@@ -100,4 +98,3 @@ while True:
         click_er(option_5_1, option_5_2, option_5_3, option_5_4)
 
         input("Press Enter to continue...")
-        
